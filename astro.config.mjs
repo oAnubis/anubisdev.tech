8596d7c6a1ddf99f8f569b4c38385a2a096e15b8
Brandon Coburn <70118929+oAnubis@users.noreply.github.com>
2022-10-01 00:32:12 -0400

    Stage2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'http://anubisdev.tech',
-    base: '/anubisdev.tech',
     experimental: {
         integrations: true,
       },
