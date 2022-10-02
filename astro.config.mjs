486f73775ec708960e5a30182cd99947141b2400
Brandon Coburn <70118929+oAnubis@users.noreply.github.com>
2022-10-02 00:07:38 -0400

    Drop down framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://anubisdev.tech',
-    experimental: {
-        integrations: true,
-      },
-    markdown: {
-        drafts: true,
-    }
+  site: 'https://anubisdev.tech',
+  experimental: {
+    integrations: true
+  },
+  markdown: {
+    drafts: true
+  },
+  integrations: [react(), tailwind()]
 });
